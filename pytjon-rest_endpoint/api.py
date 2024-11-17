0631d07436e11416e8c728cb452a0e9fcd72ae79
Pascal Scheiben <flohzirkus666@gmail.com>
2024-11-17 14:46:24 +0100

    First draft of Python API

---
+++ pytjon-rest_endpoint/api.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, status
+from pydantic import BaseModel
+
+from . import create_cifs_share
+
+
+class CifsShare(BaseModel):
+    smb_name: str
+    smb_size: int
+    smb_permission: str
+
+
+app = FastAPI()
+
+
+# API endpoint
+@app.post("/cifs_share")
+async def create_share(share: CifsShare):
+    """API Endpoint for creating a share"""
+    # calling share create
+    result = create_cifs_share(share.share_name, share.size, share.permission)
+    if result:
+        return status.HTTP_200_OK
+    else:
+        return status.HTTP_400_BAD_REQUEST
---
+++ pytjon-rest_endpoint/create_share.py
@@ -0,0 +1,48 @@
+import os
+
+from netapp_ontap import HostConnection
+from netapp_ontap.error import NetAppRestError
+from netapp_ontap.resources import CifsShare, Volume
+
+# Getting connection from env
+connection = HostConnection(
+    os.environ["ONTAP_HOST"],
+    os.environ["ONTAP_USERNAME"],
+    os.environ["ONTAP_PASSWORD"],
+    verify=False,
+)
+
+
+def create_cifs_share(
+    share_name: str,
+    size: str,
+    permissions: str,
+) -> bool:
+    """A simple example for implementing a share creation."""
+    # first step: creating a volume
+    new_vol = Volume(
+        name=f"{share_name}_vol",
+        svm={"name": "svm1"},
+        size=size,
+        nas={"path": f"/{share_name}".lower()},
+        aggregates=[{"name": "cluster1_01_aggr1"}],
+    )
+    # second step will be creating a share
+    new_share = CifsShare()
+    # we are setting here share acls according to our order
+    new_share.acls = [
+        {"permission": "full_control", "user_or_group": permissions, "type": "windows"}
+    ]
+    new_share.svm = {"name": "svm1"}
+    new_share.path = f"/{share_name}".lower()
+    new_share.name = share_name
+    with connection:
+        try:
+            # volume creation should be a blocking event
+            new_vol.post(hydrate=True, poll=True)
+            new_share.post(hydrate=True)
+            # for showing off
+            print(new_vol, new_share)
+            # if creating was successul, return true
+        except NetAppRestError:
+            return False
